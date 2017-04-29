024059a1d6fb157bbd67f67d8dc7bd07c95ae91f
TathevM <tatev.matevosyan@gmail.com>
2017-04-29 17:04:58 +0400

    lambda calculation and Projection function added,

--- Capstone/functions.py
+++ Capstone/functions.py
@@ -28,8 +28,6 @@ def logistic(x):
 """
 Log-Likelihood: Function that calculates log-likelihood of given Matrix.
 """
-
-
 def log_likelihood(X, Y, omega):
     """
   
@@ -61,6 +59,55 @@ def log_likelihood(X, Y, omega):
         gradient_log_lik += grad_elem
 
     return log_lik, gradient_log_lik
+
+"""
+Lambda function, computes lambda for Projection function
+"""
+def lamdba_x(X, r, alpha):
+    """
+
+    :param X: 
+    :param r: 
+    :param alpha: 
+    :return: 
+    """
+    U, D, V = np.linalg.svd(X)
+    d1, d2 = X.shape
+    for k in range(0, r + 1):
+        if nuclear_norm_condition(alpha, r, d1, d2, D, k):
+            lam = (np.sum(D[0:k]) - alpha * np.sqrt(r * d1 * d2)) / k
+            return U, D, V, lam
+
+
+"""
+Check the nuclear norm condition
+"""
+def nuclear_norm_condition(alpha, r, d1, d2, D, k):
+    """
+
+    :param alpha: 
+    :param r: 
+    :param d1: 
+    :param d2: 
+    :param D: 
+    :param k: 
+    :return: 
+    """
+    sum_dk1 = np.sum(D[0:k]) - k * D.item(k + 1)
+    sum_dk2 = np.sum(D[0:k]) - k * D.item(k)
+    radi = alpha * np.sqrt(r * d1 * d2)
+    if sum_dk1 >= radi and sum_dk2 <= radi:
+        return True
+    else:
+        return False
+
+
+def projection_on_set(X, r, alpha):
+    U, D, V, lam = lamdba_x(X, r, alpha)
+    lam_vector = np.ones(D.shape) * lam
+    diff_vector = D - lam_vector
+    diff_vector = diff_vector.clip(min=0)
+    return np.dot(np.dot(U, diff_vector), np.transpose(V))
 
 
 
--- Capstone/one_bit_matrix_completion.py
+++ Capstone/one_bit_matrix_completion.py
@@ -1,8 +1,10 @@
 
+from Capstone import functions as fun
 
 class OneBitMatrixCompletion:
     def __init__(self, probability_distro, theta):
         pass
 
     def complete(self, M):
+
         pass
